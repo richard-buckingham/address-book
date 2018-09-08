102d93d01fe6d22447315ea766a6c93be3b5da16
richie.buckingham <richie.buckingham@gmail.com>
2018-09-08 10:51:33 +1000

    move external interactions into address component

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 import { Contact } from "../../models/contact.model";
 
@@ -8,30 +8,9 @@ import { Contact } from "../../models/contact.model";
   styleUrls: ["./contact-list.component.scss"]
 })
 export class ContactListComponent implements OnInit {
-  private contacts: Contact[];
-
+  @Input()
+  contacts: Contact[];
   constructor() {}
 
-  ngOnInit() {
-    this.contacts = [
-      {
-        firstname: "Dan",
-        lastname: "Ricciardo",
-        department: "Marketing",
-        phonenumber: "0400111111"
-      },
-      {
-        firstname: "Helmet",
-        lastname: "Marko",
-        department: "HR",
-        phonenumber: "0400333333"
-      },
-      {
-        firstname: "Max",
-        lastname: "Verstappen",
-        department: "Marketing",
-        phonenumber: "0400222222"
-      }
-    ];
-  }
+  ngOnInit() {}
 }
--- src/app/containers/address-book/address-book.component.ts
+++ src/app/containers/address-book/address-book.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Contact } from "../../models/contact.model";
+
 @Component({
   selector: "address-book",
   templateUrl: "./address-book.component.html",
   styleUrls: ["./address-book.component.scss"]
 })
 export class AddressBookComponent implements OnInit {
+  private contacts: Contact[];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.contacts = [
+      {
+        firstname: "Dan",
+        lastname: "Ricciardo",
+        department: "Marketing",
+        phonenumber: "0400111111"
+      },
+      {
+        firstname: "Helmet",
+        lastname: "Marko",
+        department: "HR",
+        phonenumber: "0400333333"
+      },
+      {
+        firstname: "Max",
+        lastname: "Verstappen",
+        department: "Marketing",
+        phonenumber: "0400222222"
+      }
+    ];
+  }
 }
