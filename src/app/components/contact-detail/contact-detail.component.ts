d6419ba1586316636a9f707cbd78db106a9823f9
richie.buckingham <richie.buckingham@gmail.com>
2018-09-08 12:43:40 +1000

    add onPush change detection

--- src/app/components/contact-detail/contact-detail.component.ts
+++ src/app/components/contact-detail/contact-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 
 import { Contact } from "../../models/contact.model";
 
 @Component({
   selector: "contact-detail",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./contact-detail.component.html",
   styleUrls: ["./contact-detail.component.scss"]
 })
--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from "@angular/core";
 
 import { Contact } from "../../models/contact.model";
 
 @Component({
   selector: "contact-list",
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./contact-list.component.html",
   styleUrls: ["./contact-list.component.scss"]
 })
