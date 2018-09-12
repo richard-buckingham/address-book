72158cf3a75e7a66e3e3b41367d2a69663f1263f
richie.buckingham <richie.buckingham@gmail.com>
2018-09-12 18:36:12 +1000

    simple unit tests

--- src/app/services/reference-data.service.spec.ts
+++ src/app/services/reference-data.service.spec.ts
@@ -1,15 +1,26 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { ReferenceDataService } from './reference-data.service';
+import { ReferenceDataService } from "./reference-data.service";
 
-describe('ReferenceDataService', () => {
+describe("ReferenceDataService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ReferenceDataService]
     });
   });
 
-  it('should be created', inject([ReferenceDataService], (service: ReferenceDataService) => {
-    expect(service).toBeTruthy();
-  }));
+  it("should be created", inject(
+    [ReferenceDataService],
+    (service: ReferenceDataService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+
+  it("should return 7 departments", inject(
+    [ReferenceDataService],
+    (service: ReferenceDataService) => {
+      const departments = service.getDepartments();
+      expect(departments.length).toBe(7);
+    }
+  ));
 });
--- src/app/services/state.service.spec.ts
+++ src/app/services/state.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from "@angular/core/testing";
 
 import { StateService } from "./state.service";
+import { Contact } from "../models/contact.model";
 
 describe("StateService", () => {
   beforeEach(() => {
@@ -12,4 +13,42 @@ describe("StateService", () => {
   it("should be created", inject([StateService], (service: StateService) => {
     expect(service).toBeTruthy();
   }));
+
+  it("should return 5 sample contacts", inject(
+    [StateService],
+    (service: StateService) => {
+      const contacts = service.getContacts();
+      expect(contacts.length).toBe(5);
+    }
+  ));
+
+  it("should add a contact to the contact array", inject(
+    [StateService],
+    (service: StateService) => {
+      const contact: Contact = {
+        firstname: "Kimi",
+        lastname: "Rikannen",
+        department: "Racing",
+        phonenumber: "0400123456"
+      };
+
+      service.saveContact(contact);
+      const contacts = service.getContacts();
+      expect(contacts.length).toBe(6);
+    }
+  ));
+
+  it("should delete from a specified index of the contact array", inject(
+    [StateService],
+    (service: StateService) => {
+      service.deleteContact(0);
+      const contacts = service.getContacts();
+      const contact = contacts.find(
+        c => c.firstname.toLocaleLowerCase() === "dan"
+      );
+
+      expect(contacts.length).toBe(4);
+      expect(contact).toBeFalsy();
+    }
+  ));
 });
