c1c1fc35e203d521714123ce7418f7fde8ab1ebc
richie.buckingham <richie.buckingham@gmail.com>
2018-09-16 15:07:29 +1000

    update readme

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { DebugElement } from "@angular/core";
-import { By } from "@angular/platform-browser";
+  import { DebugElement } from "@angular/core";
+  import { By } from "@angular/platform-browser";
 
 import { NavBarComponent } from "./nav-bar.component";
 
